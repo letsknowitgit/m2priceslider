ade1a86d4ea3afc4d9cccdd1e40cfe7ce5b47b97
ranjeet <ranjeet.singh01@nagarro.com>
2020-01-10 01:39:51 +0530

    add composer file

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* LetsknowIT.
+*
+* @category  Letsknowit
+* @package   Letsknowit_Priceslider
+* @author    LetsknowIT
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Letsknowit_Priceslider',
+    __DIR__
+);
